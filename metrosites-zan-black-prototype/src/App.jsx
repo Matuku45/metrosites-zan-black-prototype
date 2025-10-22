8abb2ed295115375e9e701b04385d60b33d1703c
Manuel Mapoulo <manuelthabisomapoulo@gmail.com>
2025-10-22 10:39:01 +0200

    adding the skeleton of the project

--- metrosites-zan-black-prototype/src/App.jsx
+++ metrosites-zan-black-prototype/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import React from 'react'
-function App() {
 
+function App() {
 
   return (
     <>
-      <p>Hello </p>
+     <p className="text-blue-500">Hello</p>
+
     </>
   )
 }
